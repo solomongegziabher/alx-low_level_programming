205b073fd75ddbf4a4d0f3631e929d69c63264c5
solomongegziabher <sage.h22@gmail.com>
2022-10-18 22:51:39 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha - checks for alphabet
  * @c: character to be cheched
- *
- * Returns: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
 		return (1);
 	else
 		return (0);
