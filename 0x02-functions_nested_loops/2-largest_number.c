ddb98fa21c012a25d34983acbd57968dea26b267
solomongegziabher <sage.h22@gmail.com>
2022-10-19 09:02:41 -0700

    the largest number

--- 0x02-functions_nested_loops/2-largest_number.c
+++ 0x02-functions_nested_loops/2-largest_number.c
@@ -10,23 +10,25 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a > b && a > c)
+	int largest;
+
+	if (a > b)
 	{
-		largest = a;
-	}
-	else if (b > c && b > a)
-	{
-		largest = b;
-	}
-	else if (c > b)
-	{
-		largest = c;
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest = a;
+		else
+			largest = c;
+
 	}
 	else
 	{
-		largest = b;
+		if (b < c)
+			largest = c;
+		else
+			largest = b;
 	}
 
 	return (largest);
 }
-
