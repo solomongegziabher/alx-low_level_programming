894064e24ebb014489d25d80b134a7f4f4e0fbe9
solomongegziabher <sage.h22@gmail.com>
2022-10-19 01:12:45 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - print all numbers from input to 98
+ * print_to_98 - prints all numbers from input to 98
  * @n: The starting number
  */
 void print_to_98(int n)
