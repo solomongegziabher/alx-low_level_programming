c8511b6456e2fa3cf76f5357da628f73d05f65a1
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2022-10-21 08:27:32 +0300

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,15 @@
 /**
  * _isdigit - function that verifies if a character is a digit or not
  * @a: tested character
- * Return: returns 1 if upper case, if not 0
+ * Return: returns 1 if upper case, 0 if not
  */
 
 int _isdigit(int a)
 {
-	if ((a >= 48) && (a <= 57)
+	if ((a >= 48) && (a <= 58)
+			
 			return (1);
 
 			return (0);
-			}
+			
+			}			
