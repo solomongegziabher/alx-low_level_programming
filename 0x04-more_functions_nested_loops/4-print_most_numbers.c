0ffbe53fa7f16907b66af33a8bc71fd3bcb7b741
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2022-10-21 10:03:14 +0300

    believe in num and sign

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers exept 2 and 4
+ *
+ * Return: returns nothing
+ */
+void print_numbers(void)
+{
+	int number;
+	for (number = 48; number < 58; number++)
+	{
+		if ((number == 50) || (number == 52))
+		{
+			continue;
+		}
+		_putchar(number);
+	}
+	_putchar(10);
+}
