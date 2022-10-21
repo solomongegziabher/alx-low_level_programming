ac149a864ecadb7d7776bd0671df88d1b5201bc3
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2022-10-21 09:27:02 +0300

    number speaks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,20 @@
 
 /**
  * print_numbers - print numbers
- * Return: Always 0
+ * Return: Always 0.
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	i =0;
+
 	while (i < 10)
 	{
-		_putchar (i + '0');
+		_putchar(i + '0');
 		i++;
 	}
 
-	_putchar ('\n');
+	_putchar('\n');
 }
