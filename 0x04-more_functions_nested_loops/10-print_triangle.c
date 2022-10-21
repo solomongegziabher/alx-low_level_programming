770da45892fafa09037088f5ce932db33c6c036b
solomongegziabher <sage.h22@gmail.com>
2022-10-21 06:47:18 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,27 +2,32 @@
 
 /**
  * print_triangle - prints a triangle
- * @n: size of the triangle
+ * @size: Intiger type
+ * Return: void
  */
 
-void print_triangle(int n)
+void print_triangle(int size)
 {
-	int h, tri;
+	int i = 1, ii;
 
-	if (n > 0)
+	while (i <= size && size > 0)
 	{
-		for (h = 1; h <= n; h++)
+		ii = 0;
+		while (ii < size - i)
 		{
-			for (tri = n - h; tri > 0; tri--)
-				_putchar(' ');
-
-			for (tri = 0; tri < h; tri++)
-				_putchar('#');
-
-			if (h == n)
-				continue;
-
-			_putchar('\n');
+			_putchar(' ');
+			ii++;
 		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
 	}
+	if (i == 1)
+		_putchar('\n');
 }
