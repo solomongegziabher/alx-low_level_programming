f2b2da9ae53c907c493aae29af73a18ccb05dc49
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2022-10-21 08:09:08 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
- * _isupper - checkes upper case
- * @c: print int
- * Return: give 1 or 0
+ * _isupper - checks upper case
+ * @a: print int
+ * Return: return 1 or 0
  */
 
-int _isupper(int c)
+int _isupper(int a)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+	if (a >= 'A' && a <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
