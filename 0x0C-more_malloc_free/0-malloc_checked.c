a047323ee09fc9c7efca4114c2bfc99c4d08ce69
solomongegziabher <sage.h22@gmail.com>
2022-11-10 02:59:53 -0800

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,8 @@
 
 void *malloc_checked(unsigned int b)
 {
-		void *p = malloc(b);
-
-			if (p == NULL)
-				exit(98);
-
-				return (p);
+	void *p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
