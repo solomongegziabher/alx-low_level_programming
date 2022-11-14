5f40cbcdee6ec5995589b1aaf1ece9ffe2869c41
solomongegziabher <sage.h22@gmail.com>
2022-11-14 04:50:04 -0800

    perpetually wrong

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, lenN, lenO;
+
 	struct dog *n_dog = NULL;
+
 	lenN = 0;
 	while (name[lenN] != '\0')
 		lenN++;
 	lenO = 0;
 	while (owner[lenO] != '\0')
 		lenO++;
+
 	n_dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
 	{
