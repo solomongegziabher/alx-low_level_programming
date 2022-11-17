588bdcfd780150d11c84ca0d617f2a163e0b247a
solomongegziabher <sage.h22@gmail.com>
2022-11-17 04:20:11 -0800

    Variadic functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include <stdio.h>
 
 /**
@@ -10,13 +11,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	if (array == NULL || action == NULL)
+		return;
 
-	if (array && size > 0 && action)
+	while (size-- > 0)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		action(*array);
+		array++;
 	}
 }
