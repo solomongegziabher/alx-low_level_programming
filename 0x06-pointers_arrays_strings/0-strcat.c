80c814a10d3512404db4c706f3a2eebfe1373be4
solomongegziabher <sage.h22@gmail.com>
2023-01-16 10:55:07 -0800

    all task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,6 @@
 
 char *_strcat(char *dest, char *src)
 {
-
-		int i, c;
-
-			for (i = 0; dest[i] != '\0'; i++);
-
-				for (c = 0; src[c] != '\0'; c++);
-						{
-									dest[i] = src[c];
-								i++;
-						}	
-
-					dest[i] = '\0';
-						return (dest);
-
 	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
